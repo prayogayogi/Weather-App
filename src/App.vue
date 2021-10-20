5e7e9f4b78323109415e66b2e6a9dff76c46f9b6
prayogayogi <yogikpiter@gmail.com>
2021-10-21 01:29:37 +0700

    Done Weather App

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <judul/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Judul from './components/Judul.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Judul
   }
 }
 </script>
---
+++ src/components/Judul.vue
@@ -0,0 +1,62 @@
+<template>
+  <div id="app">
+    <div class="container">
+    <h1>Weather App</h1>
+      <div class="row justify-content-center">
+        <div class="col-10 mt-2">
+          <div class="search">
+            <input type="text" name="Search" v-model="cari" autofocus class="form-control" @keypress="dataSuhu" placeholder="Cari....">
+          </div>
+        </div>
+      </div>
+      <div class="row mt-3" v-if="typeof suhu.main != 'undefined'">
+        <div class="col">
+          <h4 class="lokasi">{{ suhu.name }}, {{ suhu.sys.country }}</h4>
+          <p class="tanggal">{{ dat }}</p>
+          <h2 class="suhu">{{ Math.round(suhu.main.temp) }}'C</h2>
+          <h3 class="status">{{ suhu.weather[0].main }}</h3>
+        </div>
+      </div>
+      <div class="not" v-else>
+        <p class="mt-4">Silahkan Masukan Negara Yang Anda Inggin Anda Cari.!</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import bootstrap from 'bootstrap/dist/css/bootstrap.css'
+import axios from 'axios'
+
+export default {
+  name:"judul",bootstrap,
+  data() {
+    return {
+      urlApi: 'https://api.openweathermap.org/data/2.5/',
+      apiKey: '8a5a0fc6ec28d0edc29b6c280f4ffca7',
+      cari: '',
+      suhu:{},
+      dat: ''
+    }
+  },
+  methods:{
+    dataSuhu (el) {
+      if(el.key == 'Enter'){
+        axios.get(`${this.urlApi}weather?q=${this.cari}&units=metric&APPID=${this.apiKey}`)
+        .then(res => {
+          this.suhu = res.data
+        })
+      }
+      this.dat = new Date().toLocaleString()
+    }
+  }
+}
+</script>
+
+<style scoped>
+.container{
+  border: 1px solid #334756;
+  width: 20%;
+  padding: 20px;
+}
+</style>
